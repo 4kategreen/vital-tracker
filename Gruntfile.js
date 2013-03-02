a7ce51af0a222db44825115a442b56a1d401ef8c
4kategreen <kate@kategreen.us>
2013-03-02 17:40:33 -0500

    Adding and modifying housekeeping files.

---
+++ Gruntfile.js
@@ -0,0 +1,29 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json');
+    less: {
+    	'bootstrap': {
+		  	options: {
+		      paths: ["bootstrap/less"]
+		    },
+		    files: {
+		      "css/bootstrap.css": "bootstrap/less/bootstrap.less",
+		      "css/bootstrap-responsive.css": "boostrap/less/bootstrap-responsive.less"
+		    },
+		    'vitals': {
+		    	options: {
+		    		paths: ["less"]
+		    	},
+		    	files: {
+		    		"css/vitals.css": "less/vitals.less",
+		    	}
+		    }
+    },
+    jshint: {
+      files: ['Gruntfile.js','app/**/*.js','js/**/*.js','test/**/*.js'],
+    }
+
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+  });
+};
