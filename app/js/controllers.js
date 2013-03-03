03bb195569b3ae7fcc6db1e22c2d4da6f478a186
4kategreen <kate@kategreen.us>
2013-03-03 15:00:30 -0500

    got parse sending data to angular. so basically, it works for pulling and displaying data.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,12 +1,37 @@
 'use strict';
+Parse.initialize('0S7MAOMdSUPRcKolXs3BNPV4asdO4dnS7elfK53C','r3EqOgzYMfVz5wQAPAsNL8nDGp8BwSkLjmR8PMsE');
+var Vitals = Parse.Object.extend('vitals');
 
 /* Controllers */
+function VitalsCtrl($scope) {
 
+	function getVitals() {
+		var vitals = new Parse.Query(Vitals);
+		vitals.ascending('date');
+	  vitals.find({
+	    success: function(results) {
+	      $scope.$apply(function() {
+	      	$scope.vitals = results.map(function(obj) {
+	      		return {
+	      			weight: obj.get('weight'),
+	      			fat: obj.get('fat'),
+	      			date: parseDate(obj.get('date')),
+	      			rhr: obj.get('rhr')
+	      		}
+	      	});
+	      });
+	    },
+	    error: function(error) {
+	      console.log(error);
+	    }
+	  });
+	}
+	function parseDate(d) {
+		var date = new Date(d);
+		return ((date.getMonth() + 1) + "/" + date.getDate() + "/" + date.getFullYear());
 
-function MyCtrl1() {}
-MyCtrl1.$inject = [];
-
-
-function MyCtrl2() {
+		//{"__type":"Date","iso":"2013-03-01T20:26:00.000Z"}
+	}
+	getVitals();
+	$scope.title = 'Vitals';
 }
-MyCtrl2.$inject = [];
--- js/vitals.js
+++ js/vitals.js
@@ -1,14 +1,18 @@
-(function() {
+var parse = function() {
   Parse.initialize('0S7MAOMdSUPRcKolXs3BNPV4asdO4dnS7elfK53C','r3EqOgzYMfVz5wQAPAsNL8nDGp8BwSkLjmR8PMsE');
+
 
   var Vitals = Parse.Object.extend('vitals');
   var vitals = new Parse.Query(Vitals);
   vitals.find({
     success: function(results) {
-      $('.data').append(results);
+      for (var r in results) {
+        //$('.data').append(JSON.stringify(results[r])+'<br/>');
+        //console.log(results[r]);
+      }
     },
     error: function(error) {
       console.log(error);
     }
   });
-})();
+};
