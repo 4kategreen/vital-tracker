167a721247a2be8eac033d86c21e3a4d6e5622ac
4kategreen <kate@kategreen.us>
2013-03-11 13:40:26 -0400

    Adding fitbit creds in an object for now.

---
+++ app/js/fitbit.js
@@ -0,0 +1,7 @@
+var Fitbit = {
+  consumerKey: 'f159ccf73f8c43ac910da23dd29e504c',
+  consumerSecret: 'a334c5c371b34f4eb506595eedefee3a',
+  requestTokenURL: 'http://api.fitbit.com/oauth/request_token',
+  accessTokenURL: 'http://api.fitbit.com/oauth/access_token',
+  authorizeURL: 'http://www.fitbit.com/oauth/authorize'
+}
